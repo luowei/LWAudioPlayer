1af5fef406ec532e9d76d4bb27fc743f6358d86a
luowei <luowei@wodedata.com>
2019-12-08 23:36:28 +0800

    add pub_podspec.sh

---
+++ pub_podspec.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pod repo push mygitlabrepo LWAudioPlayer.podspec --verbose --allow-warnings --sources="https://gitlab.com/ioslibraries1/mygitlabrepo.git,https://github.com/CocoaPods/Specs.git"
